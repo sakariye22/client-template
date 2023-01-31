f0e148b8269559bb1f9e03d918838e96dc6b4865
zakariye <zakariye1942@icloud.com>
2023-01-31 10:22:01 +0100

    copy

---
+++ index.js
@@ -0,0 +1,16 @@
+/*const express = require ('express');
+const app = express();
+const bodyParser = require('body-parser');
+import {getAllClients} from "server.js";
+
+
+app.use (bodyParser.json());
+
+
+
+app.get('/our', (req, res) => {
+    res.send('working');
+});
+app.listen(4000);
+
+    */
